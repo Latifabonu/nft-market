2c5045fb7797c38c41d6be7505095050229c7ffe
Latifabonu <Your Name>
2023-12-25 16:46:24 +0500

    new change

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
 const menuBtn = document.getElementById("menu-btn");
-const closeBtn = document.getElementById("close-menu");
+const closeBtn = document.getElementById("close-btn");
 const mobileNav = document.getElementById("mobile-nav");
 
-
-menuBtn.addEventListener("click", ()=> {
-    mobileNav.classList.add("top-0");
+menuBtn.addEventListener("click", ()=>{
+    mobileNav.classList.add("top-0")
     mobileNav.classList.remove("top-[-100%]");
 })
 
-closeBtn.addEventListener("click", ()=> {
+closeBtn.addEventListener("click", ()=>{
+    mobileNav.classList.remove("top-0")
     mobileNav.classList.add("top-[-100%]");
-    mobileNav.classList.remove("top-0");
 })
